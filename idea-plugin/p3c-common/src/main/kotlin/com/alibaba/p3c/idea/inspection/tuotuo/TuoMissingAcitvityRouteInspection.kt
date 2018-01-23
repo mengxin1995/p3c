404ee460763bab85fe16c24af8a47f7c7acd7f82
mengxin <mengxin@tuotuonet.com>
2018-01-23 17:49:50 +0800

    update 代码解释

--- idea-plugin/p3c-common/src/main/kotlin/com/alibaba/p3c/idea/inspection/tuotuo/TuoMissingAcitvityRouteInspection.kt
+++ idea-plugin/p3c-common/src/main/kotlin/com/alibaba/p3c/idea/inspection/tuotuo/TuoMissingAcitvityRouteInspection.kt
@@ -12,11 +12,57 @@ import com.siyeh.ig.BaseInspectionVisitor
 import org.apache.http.util.TextUtils
 import org.intellij.plugins.relaxNG.compact.psi.RncElementVisitor
 
+/*
+* 这边要继承BaseInspection,AliBaseInspection
+* BaseInspection是IDEA源码里面的基础类
+* AliBaseInspection是阿里提供的基础类
+* 这两个类是一定要实现的
+* */
 class TuoMissingAcitvityRouteInspection : BaseInspection, AliBaseInspection {
+
+    private val messageKey = "com.alibaba.p3c.idea.inspection.tuotuo.TuoMissingAcitvityRouteInspection"
+
+    constructor()
+
+    /**
+     * For Javassist
+     */
+    constructor(any: Any?) : this()
+
+    //规程的标题
+    override fun ruleName(): String {
+        return "MissingAcitvityRouteRule"
+    }
+
+    //错误信息
+    override fun buildErrorString(vararg infos: Any): String = P3cBundle.getMessage("$messageKey.errMsg")
+
+    //错误提示标题
+    override fun getDisplayName(): String = "Your activity should add route"
+
+    //错误提示内容体
+    override fun getStaticDescription(): String? = "Your activity should add route !!!"
+
+    //错误的等级
+    override fun getDefaultLevel(): HighlightDisplayLevel = HighlightDisplayLevels.CRITICAL
+
+    //规则的短标题
+    override fun getShortName(): String = "TuoMissingAcitvityRoute"
+
+    //是否默认开启
+    override fun isEnabledByDefault(): Boolean = true
+
+    //回调一个遍历的类，这个类是由IDEA自己去控制的
     override fun buildVisitor(): BaseInspectionVisitor {
         return MissingActivityRouteVisitor()
     }
 
+    /*
+    * 这里是规则的具体写法，比较灵活，自己的业务可以看一下别的规则的实现来推导自己的实现
+    * 我这里只要是如果这个类名字里面包含了Activity，就去拿它的注解，
+    * 如果注解为空，或者说注解列表里面不包含"com.alibaba.android.arouter.facade.annotation.Route"
+    * 即表示没有添加过路由，就会通过registerClassError(aClass)输出错误信息
+    * */
     class MissingActivityRouteVisitor : BaseInspectionVisitor() {
         override fun visitClass(aClass: PsiClass?) {
             super.visitClass(aClass)
@@ -39,32 +85,5 @@ class TuoMissingAcitvityRouteInspection : BaseInspection, AliBaseInspection {
             return annotation != null
         }
     }
-
-
-    private val messageKey = "com.alibaba.p3c.idea.inspection.tuotuo.TuoMissingAcitvityRouteInspection"
-
-    constructor()
-
-    /**
-     * For Javassist
-     */
-    constructor(any: Any?) : this()
-
-    override fun ruleName(): String {
-        return "MissingAcitvityRouteRule"
-    }
-
-    //错误信息
-    override fun buildErrorString(vararg infos: Any): String = P3cBundle.getMessage("$messageKey.errMsg")
-
-    override fun getDisplayName(): String = "Your activity should add route"
-
-    override fun getStaticDescription(): String? = "Your activity should add route !!!"
-
-    override fun getDefaultLevel(): HighlightDisplayLevel = HighlightDisplayLevels.CRITICAL
-
-    override fun getShortName(): String = "TuoMissingAcitvityRoute"
-
-    override fun isEnabledByDefault(): Boolean = true
 
 }
