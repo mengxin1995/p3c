b85fa9ff33fc3f96b64379ea65d5ca87f942a738
mengxin <mengxin@tuotuonet.com>
2018-01-23 16:57:49 +0800

    update 修复插件装上后不能使用

--- idea-plugin/p3c-common/src/main/kotlin/com/alibaba/p3c/idea/inspection/tuotuo/TuoMissingAcitvityRouteInspection.kt
+++ idea-plugin/p3c-common/src/main/kotlin/com/alibaba/p3c/idea/inspection/tuotuo/TuoMissingAcitvityRouteInspection.kt
@@ -2,11 +2,44 @@ package com.alibaba.p3c.idea.inspection.tuotuo
 
 import com.alibaba.p3c.idea.i18n.P3cBundle
 import com.alibaba.p3c.idea.inspection.AliBaseInspection
-import com.alibaba.p3c.idea.inspection.tuotuo.base.MissingActivityRouteInspection
 import com.alibaba.p3c.idea.util.HighlightDisplayLevels
 import com.intellij.codeHighlighting.HighlightDisplayLevel
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiModifierListOwner
+import com.siyeh.ig.BaseInspection
+import com.siyeh.ig.BaseInspectionVisitor
+import org.apache.http.util.TextUtils
+import org.intellij.plugins.relaxNG.compact.psi.RncElementVisitor
 
-class TuoMissingAcitvityRouteInspection : MissingActivityRouteInspection, AliBaseInspection {
+class TuoMissingAcitvityRouteInspection : BaseInspection, AliBaseInspection {
+    override fun buildVisitor(): BaseInspectionVisitor {
+        return MissingActivityRouteVisitor()
+    }
+
+    class MissingActivityRouteVisitor : BaseInspectionVisitor() {
+        override fun visitClass(aClass: PsiClass?) {
+            super.visitClass(aClass)
+            if (aClass == null) return
+
+            if (TextUtils.isEmpty(aClass.name) || !aClass.name!!.contains("Activity")) {
+                return
+            }
+
+            if (hasRouteOverrideAnnotation(aClass)) {
+                return
+            }
+            registerClassError(aClass)
+        }
+
+        private fun hasRouteOverrideAnnotation(
+                element: PsiModifierListOwner): Boolean {
+            val modifierList = element.modifierList ?: return false
+            val annotation = modifierList.findAnnotation("com.alibaba.android.arouter.facade.annotation.Route")
+            return annotation != null
+        }
+    }
+
 
     private val messageKey = "com.alibaba.p3c.idea.inspection.tuotuo.TuoMissingAcitvityRouteInspection"
 
